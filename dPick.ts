a0e8651a621718a6905d29edd7c5f0110364960c
Siddharth Gelera <sidgroks@gmail.com>
2018-08-28 13:07:38 +0530

    namespace

--- dPick.ts
+++ dPick.ts
@@ -37,13 +37,13 @@ const htmlTemplate = `
                   styles.col +
                   styles.center +
                   styles.pSm}">
-                    <div class="up p-sm" style="${styles.pSm}">
+                    <div class="dpickr-up" style="${styles.pSm}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M11.574 3.712c.195-.323.662-.323.857 0l9.37 15.545c.2.333-.039.757-.429.757l-18.668-.006c-.385 0-.629-.422-.428-.758l9.298-15.538zm.429-2.483c-.76 0-1.521.37-1.966 1.111l-9.707 16.18c-.915 1.523.182 3.472 1.965 3.472h19.416c1.783 0 2.879-1.949 1.965-3.472l-9.707-16.18c-.446-.741-1.205-1.111-1.966-1.111z"/></svg>
                     </div>
-                    <div class="date">
+                    <div class="dpickr-date">
 
                     </div>
-                    <div class="down" style="transform:rotate(180deg);${
+                    <div class="dpickr-down" style="transform:rotate(180deg);${
                       styles.pSm
                     }">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M11.574 3.712c.195-.323.662-.323.857 0l9.37 15.545c.2.333-.039.757-.429.757l-18.668-.006c-.385 0-.629-.422-.428-.758l9.298-15.538zm.429-2.483c-.76 0-1.521.37-1.966 1.111l-9.707 16.18c-.915 1.523.182 3.472 1.965 3.472h19.416c1.783 0 2.879-1.949 1.965-3.472l-9.707-16.18c-.446-.741-1.205-1.111-1.966-1.111z"/></svg>
@@ -53,13 +53,13 @@ const htmlTemplate = `
                   styles.col +
                   styles.center +
                   styles.pSm}">
-                    <div class="up p-sm" style="${styles.pSm}">
+                    <div class="dpickr-up" style="${styles.pSm}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M11.574 3.712c.195-.323.662-.323.857 0l9.37 15.545c.2.333-.039.757-.429.757l-18.668-.006c-.385 0-.629-.422-.428-.758l9.298-15.538zm.429-2.483c-.76 0-1.521.37-1.966 1.111l-9.707 16.18c-.915 1.523.182 3.472 1.965 3.472h19.416c1.783 0 2.879-1.949 1.965-3.472l-9.707-16.18c-.446-.741-1.205-1.111-1.966-1.111z"/></svg>
                     </div>
-                    <div class="date">
+                    <div class="dpickr-date">
 
                     </div>
-                    <div class="down" style="transform:rotate(180deg);${
+                    <div class="dpickr-down" style="transform:rotate(180deg);${
                       styles.pSm
                     }">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M11.574 3.712c.195-.323.662-.323.857 0l9.37 15.545c.2.333-.039.757-.429.757l-18.668-.006c-.385 0-.629-.422-.428-.758l9.298-15.538zm.429-2.483c-.76 0-1.521.37-1.966 1.111l-9.707 16.18c-.915 1.523.182 3.472 1.965 3.472h19.416c1.783 0 2.879-1.949 1.965-3.472l-9.707-16.18c-.446-.741-1.205-1.111-1.966-1.111z"/></svg>
@@ -69,13 +69,13 @@ const htmlTemplate = `
                   styles.col +
                   styles.center +
                   styles.pSm}">
-                    <div class="up p-sm" style="${styles.pSm}">
+                    <div class="dpickr-up" style="${styles.pSm}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M11.574 3.712c.195-.323.662-.323.857 0l9.37 15.545c.2.333-.039.757-.429.757l-18.668-.006c-.385 0-.629-.422-.428-.758l9.298-15.538zm.429-2.483c-.76 0-1.521.37-1.966 1.111l-9.707 16.18c-.915 1.523.182 3.472 1.965 3.472h19.416c1.783 0 2.879-1.949 1.965-3.472l-9.707-16.18c-.446-.741-1.205-1.111-1.966-1.111z"/></svg>
                     </div>
-                    <div class="date">
+                    <div class="dpickr-date">
 
                     </div>
-                    <div class="down" style="transform:rotate(180deg);${
+                    <div class="dpickr-down" style="transform:rotate(180deg);${
                       styles.pSm
                     }">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M11.574 3.712c.195-.323.662-.323.857 0l9.37 15.545c.2.333-.039.757-.429.757l-18.668-.006c-.385 0-.629-.422-.428-.758l9.298-15.538zm.429-2.483c-.76 0-1.521.37-1.966 1.111l-9.707 16.18c-.915 1.523.182 3.472 1.965 3.472h19.416c1.783 0 2.879-1.949 1.965-3.472l-9.707-16.18c-.446-.741-1.205-1.111-1.966-1.111z"/></svg>
@@ -121,9 +121,9 @@ class dPick {
   private addSelectors() {
     forEach(this.selectors, (key: string, value: any) => {
       const el = elemGrab(this.options.el, ".dpickr-" + key);
-      const upButton = elemGrab(el, ".up");
-      const downButton = elemGrab(el, ".down");
-      this.selectors[key] = elemGrab(el, ".date");
+      const upButton = elemGrab(el, ".dpickr-up");
+      const downButton = elemGrab(el, ".dpickr-down");
+      this.selectors[key] = elemGrab(el, ".dpickr-date");
 
       upButton.addEventListener("click", (e: any) => {
         this.setTime(key, 1);
